06a1dd2cedbfdd4d86c9750bafe9b5785c5e26a4
58160639 <58160639@go.buu.ac.th>
2017-09-12 14:44:05 +0700

    test past

--- tennis-workshop.spec.js
+++ tennis-workshop.spec.js
@@ -1,4 +1,15 @@
+function TennisGame(){
+  this.reset = () =>{
 
+ }
+  this.echo = () =>{
+    return 'Love - Love'
+  }
+}
+test('try success test case',() =>{
+
+  expect(true).toBe(true)
+})
 test('Echo "Love - Love"when game start', () => {
 
 
@@ -6,10 +17,9 @@ let app = new TennisGame ()
 app.reset()
 
 
-
 let result = app.echo()
 
 
-
+ 
  expect(result).toBe('Love - Love')
 })
